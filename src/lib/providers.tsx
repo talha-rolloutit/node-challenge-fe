e21c3df860c1edf74f84e60e5dc0d692124bb471
talha-rolloutit <talha.waheed6618@gmail.com>
2024-05-26 14:10:28 +0500

    added some ui components

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,10 +1,6 @@
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
